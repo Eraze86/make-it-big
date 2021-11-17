d3e58f7e3a1449a5b96872d24be7ef16dd02f0fc
Eraze86 <therese.rosvall@hotmail.se>
2021-11-17 17:14:43 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// const body = document.body;
+
+// const h1 = document.createElement("h1");
+// h1.innerText = "Hej! vad heter du?";
+// body.append(h1);
+
+
+
+const bigname = function(name){
+
+return printname;
+};
+const printname = bigname;
+console.log(printname);

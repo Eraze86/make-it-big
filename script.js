86da6f0d7866789ea9f5f48fa2204a88822e9478
Eraze86 <therese.rosvall@hotmail.se>
2021-11-18 10:33:10 +0100

    fixed the function

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-// const body = document.body;
-
-// const h1 = document.createElement("h1");
-// h1.innerText = "Hej! vad heter du?";
-// body.append(h1);
 
 
+const name = document.getElementById("name");
 
-const bigname = function(name){
 
-return printname;
-};
-const printname = bigname;
-console.log(printname);
+const button = document.getElementById("button");
+
+button.addEventListener("click", function(){
+    name.value = name.value.toUpperCase()
+});
+
